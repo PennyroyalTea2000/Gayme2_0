183504fc21b3c014dfe6143e32ab939ef7350bd7
vadim8960 <akimovvadim779@gmail.com>
2018-12-01 22:45:48 +0300

    Start work with class BasicItem

---
+++ Linux/include/Header.h
@@ -0,0 +1,13 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <ctime>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#endif /*_HEADER_H_*/
