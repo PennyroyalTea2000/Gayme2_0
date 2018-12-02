a00cefa9f08be4c4f4303c905a6d0c87ccf4845d
vadim8960 <akimovvadim779@gmail.com>
2018-12-02 14:49:46 +0300

    Added getters for class BasicCharecter

--- Linux/include/BasicCharecter.h
+++ Linux/include/BasicCharecter.h
@@ -3,14 +3,36 @@
 
 #include "Header.h"
 
+struct Pattern {
+  unsigned max;
+  int current;
+  unsigned regeneration;
+  void normalization();
+};
+
 class BasicCharecter {
 protected:
   string name;
-  unsigned count_cell;
+  Pattern hp;
+  Pattern energy;
+  unsigned max_cell;
+  unsigned current_cell;
   unsigned count_money;
-  
-public:
 
+public:
+  BasicCharecter(string name, Pattern hp, Pattern energy,
+                 unsigned max_cell, unsigned current_cell,
+                 unsigned count_money);
+  string  getName();
+  Pattern getHP();
+  Pettern getEnergy();
+  unsigned getCurrentCell();
+  unsigned getCountMoney();
+
+  void setHP(unsigned new_max, unsigned new_regeneration);
+  void setEnergy(unsigned new_max, unsigned new_regeneration);
+  void setMaxCell(unsigned new_max);
+  void setCountMoney(unsigned new_count);
 };
 
 #endif /*_BASIC_CHARECTER_H_*/
--- Linux/src/BasicCharecter.cpp
+++ Linux/src/BasicCharecter.cpp
@@ -1,1 +1,42 @@
 #include "../include/BasicCharecter.h"
+
+void Pattern::normalization() {
+  current = (max < current) ? max : current;
+  current = (current < 0) ? 0 : current;
+}
+
+BasicCharecter::BasicCharecter(string name, Pattern hp, Pattern energy,
+                               unsigned max_cell, unsigned current_cell,
+                               unsigned count_money)
+{
+  this.name         = name;
+  this.hp           = hp;
+  this.energy       = energy;
+  this.max_cell     = max_cell;
+  this.current_cell = current_cell;
+  this.count_money  = count_money;
+}
+
+string BasicCharecter::getName() {
+  return name;
+}
+
+Pattern BasicCharecter::getHP() {
+  return hp;
+}
+
+Pattern BasicCharecter::getEnergy() {
+  return energy;
+}
+
+unsigned BasicCharecter::getMaxCell() {
+  return max_cell;
+}
+
+unsigned BasicCharecter::getCurrentCell() {
+  return current_cell;
+}
+
+unsigned BasicCharecter::getCountMoney() {
+  return count_money;
+}
